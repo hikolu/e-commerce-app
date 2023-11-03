b0c9d31ecbd3941569c86a0e055e72baa0e4eb89
Dzmitry Yemelyantsau <ddmitri280404@gmail.com>
2023-11-03 10:36:44 +0100

    Add a profile controller and profile html page

---
+++ src/main/java/com/hikolu/ecommerceapp/controller/ProfileController.java
@@ -0,0 +1,39 @@
+package com.hikolu.ecommerceapp.controller;
+
+import com.hikolu.ecommerceapp.dto.UserDTO;
+import com.hikolu.ecommerceapp.model.User;
+import com.hikolu.ecommerceapp.model.UserMapper;
+import com.hikolu.ecommerceapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@Controller
+public class ProfileController {
+
+    private UserService userService;
+
+    @Autowired
+    public ProfileController(UserService userService) {
+        this.userService = userService;
+    }
+
+    // expose profile endpoint
+    @GetMapping("/profile")
+    public String getProfile(Model model, Principal principal) {
+
+        // get user from principal
+        String username = principal.getName();
+        User dbUser = userService.getUserByUsername(username);
+        UserDTO userDTO = UserMapper.mapToUserDTO(dbUser);
+
+        // add attribute to the model
+        model.addAttribute("user", userDTO);
+
+        // return page
+        return "profile-main";
+    }
+}
