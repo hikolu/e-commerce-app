4be20c320b192d1c203c2b9f39963dc5d78aee0e
Dzmitry Yemelyantsau <ddmitri280404@gmail.com>
2023-11-10 00:08:03 +0100

    Update controllers for store and order

--- src/main/java/com/hikolu/ecommerceapp/controller/OrderController.java
+++ src/main/java/com/hikolu/ecommerceapp/controller/OrderController.java
@@ -2,10 +2,13 @@ package com.hikolu.ecommerceapp.controller;
 
 import com.hikolu.ecommerceapp.model.Order;
 import com.hikolu.ecommerceapp.service.OrderService;
+import com.hikolu.ecommerceapp.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/order")
@@ -13,19 +16,22 @@ public class OrderController {
 
     // add a service
     private OrderService orderService;
+    private ProductService productService;
 
     // use constructor to inject dependency for service
     @Autowired
-    public OrderController(OrderService orderService) {
+    public OrderController(OrderService orderService, ProductService productService) {
         this.orderService = orderService;
+        this.productService = productService;
     }
 
     // expose GET for a form
     @GetMapping
-    public String getFormAdd(Model model) {
+    public String getFormAdd(Model model, @RequestParam("productId") int productId) {
 
         // create an empty order object
         Order order = new Order();
+        order.setProductId(productId);
 
         // order to the model
         model.addAttribute("order", order);
@@ -36,10 +42,15 @@ public class OrderController {
 
     // expose POST to add new order
     @PostMapping("/save")
-    public String addOrder(@ModelAttribute("order") Order order) {
+    public String addOrder(@ModelAttribute("order") Order order, Principal principal) {
 
         // change the id (if was added) to 0, to add a new one and not to update existing one
         order.setOrderId(0);
+        order.setUsername(principal.getName());
+
+        // get the price from product and update bill on order
+        double priceForOne = productService.getProductById(order.getProductId()).getPrice();
+        order.setBill(priceForOne * order.getQuantity());
 
         // save order
         orderService.saveOrder(order);
--- src/main/java/com/hikolu/ecommerceapp/controller/StoreController.java
+++ src/main/java/com/hikolu/ecommerceapp/controller/StoreController.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -40,6 +42,7 @@ public class StoreController {
         return "/store/main-page";
     }
 
+    // expose "/store" endpoint
     @GetMapping("/store")
     public String getStoreList(Model model) {
 
@@ -54,6 +57,44 @@ public class StoreController {
     }
 
     // expose "/best-sellers" endpoint
+    @GetMapping("/best-sellers")
+    public String getBestSellers(Model model) {
 
-    // expose "/store" endpoint
+        // get products with most owners
+        List<ProductDTOMain> products = productService.getBestSellers();
+
+        // add attribute
+        model.addAttribute("products", products);
+
+        // return page
+        return "/store/best-sellers";
+    }
+
+    // expose GET "/store/create" endpoint to show form for adding
+    @GetMapping("/store/create")
+    public String getFormAddProduct(Model model) {
+
+        // create empty product object
+        Product product = new Product();
+
+        // add product to the model
+        model.addAttribute("product", product);
+
+        // return the view
+        return "/store/addProductForm";
+    }
+
+    // expose POST "/store/save" to add a new object
+    @PostMapping("/store/save")
+    public String save(@ModelAttribute("product") Product product) {
+
+        // set the id to 0 just in case the add an object with id (so that it is not updated)
+        product.setProductId(0);
+
+        // save the product
+        productService.saveProduct(product);
+
+        // redirect
+        return "redirect:/";
+    }
 }
