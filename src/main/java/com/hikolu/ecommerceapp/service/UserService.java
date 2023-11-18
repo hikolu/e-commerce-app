c7a89ec863b25a0a56bbdead7c7f18797d359d7c
Dzmitry Yemelyantsau <ddmitri280404@gmail.com>
2023-11-18 15:30:11 +0100

    Update user service

--- src/main/java/com/hikolu/ecommerceapp/service/UserService.java
+++ src/main/java/com/hikolu/ecommerceapp/service/UserService.java
@@ -4,7 +4,7 @@ import com.hikolu.ecommerceapp.model.User;
 
 import java.util.List;
 
-public interface UserService {
+public interface UserService{
 
     // get all users
     List<User> getUsers();
