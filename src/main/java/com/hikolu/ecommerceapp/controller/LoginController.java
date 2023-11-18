b862c0da103df6291c72e6d445cd27c1aea00638
Dzmitry Yemelyantsau <ddmitri280404@gmail.com>
2023-11-18 15:31:00 +0100

    Add login controller

---
+++ src/main/java/com/hikolu/ecommerceapp/controller/LoginController.java
@@ -0,0 +1,20 @@
+package com.hikolu.ecommerceapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login")
+    public String showLoginPage() {
+
+        return "login/login-page";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+
+        return "login/access-denied";
+    }
+}
