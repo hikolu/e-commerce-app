fc74ecfd4ae73b9ce69c6c5b5e34b855c55dd476
Dzmitry Yemelyantsau <ddmitri280404@gmail.com>
2023-11-22 11:43:26 +0100

    Update SecurityConfig

--- src/main/java/com/hikolu/ecommerceapp/config/SecurityConfig.java
+++ src/main/java/com/hikolu/ecommerceapp/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package com.hikolu.ecommerceapp.config;
 
+import com.hikolu.ecommerceapp.service.UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -13,6 +16,24 @@ import javax.sql.DataSource;
 @Configuration
 public class SecurityConfig {
 
+    // define bcrypt bean
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+
+        return new BCryptPasswordEncoder();
+    }
+
+    // define DaoAuth. Provider
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(UserService userService) {
+
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+
+        return auth;
+    }
+
     // get user details from the database
     @Bean
     public UserDetailsManager userDetailsManager(DataSource dataSource) {
@@ -20,14 +41,14 @@ public class SecurityConfig {
         return new JdbcUserDetailsManager(dataSource);
     }
 
-    //     set filter chain
+    // set filter chain
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.authorizeHttpRequests(configurer ->
                 configurer
                         .requestMatchers("/").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/store/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/store/**").hasAnyRole(  "ADMIN")
                         .requestMatchers("/profile/**").hasAnyRole("USER", "ADMIN")
                         .requestMatchers("/order/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
